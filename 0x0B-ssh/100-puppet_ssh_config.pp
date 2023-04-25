afdcc2c9296873a257db99d2ef942981b1589f70
BRIGHT AGGREY <113631335+stg246@users.noreply.github.com>
2023-04-25 17:20:44 +0000

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,7 @@
-file { '/root/.ssh/config':
-  content => "Host example.com\n\
-              IdentityFile ~/.ssh/school\n\
-              PasswordAuthentication no\n",
+# this is a puppet script to create and modify the ssh config file in the etc directory
+file { '/etc/ssh/sshd_config':
+  ensure  => present,
+  mode    => '0600',
+  content => "Host *\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
 }
 
