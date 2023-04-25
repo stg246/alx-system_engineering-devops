e069ecd964cbc9c4558a112024fe8ef1083d694b
Bright Aggrey <stg246activation@gmail.com>
2023-04-25 17:13:09 +0000

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,46 +1,6 @@
-#!/usr/bin/env bash
-
-# This script install Puppet on the system
-
-package { 'openssh-client':
-  ensure => installed,
+file { '/root/.ssh/config':
+  content => "Host example.com\n\
+              IdentityFile ~/.ssh/school\n\
+              PasswordAuthentication no\n",
 }
 
-
-# This script modify the SSH client configuration file
-
-file { '/etc/ssh/ssh_config':
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => template('ssh_config.erb'),
-}
-
-
-# This script create a template file (ssh_config.erb) that contains the configuration options
-
-Host *
-  IdentityFile ~/.ssh/school
-  PasswordAuthentication no
-
-
-# This script displays the full Puppet manifest
-
-package { 'openssh-client':
-  ensure => installed,
-}
-
-file { '/etc/ssh/ssh_config':
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => template('ssh_config.erb'),
-}
-
-# Template file
-file { '/etc/puppetlabs/code/environments/production/modules/ssh/templates/ssh_config.erb':
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "Host *\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
-}
