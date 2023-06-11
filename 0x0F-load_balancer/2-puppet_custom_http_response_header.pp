8a4e3018b421c4472f474e9c99a1446fcb2ee36a
Bright Aggrey <stg246activation@gmail.com>
2023-06-11 13:59:14 +0000

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,25 @@
-# Automation: creates a custom HTTP header response with Puppet.
-exec { 'command':
-  command  => 'apt-get -y update;
-  apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-  service nginx restart',
-  provider => shell,
+# automating nginx to return an HTTP Response HEADER X-Served-By
+exec { 'update_apt_cache':
+  command => '/usr/bin/apt-get update',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+package { 'nginx':
+  ensure => installed,
+}
+
+$hostname = $facts['hostname']
+
+$file_content = "add_header X-Served-By ${hostname};\n"
+
+file_line { 'add_x_server_by_header':
+  path => '/etc/nginx/sites-available/default',
+  line => $file_content,
+  match => '^\s*server_name\s',
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
+  subscribe => File_line['add_x_server_by_header'],
 }
